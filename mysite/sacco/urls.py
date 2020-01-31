7090da731d3116e5b84d30e98428b33239b67abf
costance <costancenamukwaya7@gmail.com>
2020-01-31 17:22:36 +0300

    initial commit

--- mysite/sacco/urls.py
+++ mysite/sacco/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signIn/', views.signIn, name='signIn'),
+    path('logIn/', views.logIn, name='logIn'),
+    path('detail/', views.detail, name='detail'),
 ]
